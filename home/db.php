4b0f2d77b2bb69768df821c22eeff2404d10fa06
edsine <88459473+edsine@users.noreply.github.com>
2023-07-01 11:23:19 +0100

    Update db.php
    Updated

--- home/db.php
+++ home/db.php
@@ -1,8 +1,8 @@
 <?php
        $servername='localhost';
     $username='root';
-    $password='Mkpanama1';
-    $dbname = "ebs";
+    $password='';
+    $dbname = "ebsdb";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
